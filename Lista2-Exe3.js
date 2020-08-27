83f06bd2b201adac90fccabc5fbff696275d4c2d
João Costa <josercosta0223@gmail.com>
2020-08-26 22:22:19 -0300

    Exercicio 3 - Criando...

---
+++ Lista2-Exe3.js
@@ -0,0 +1,26 @@
+chefe()
+
+function chefe(){
+    var vetor = []
+    entrada(vetor)
+    relat1(vetor)
+    relat2(vetor)
+    relat3(vetor)
+    relat4(vetor)
+}
+
+function relat1(vet){
+
+}
+
+function relat2(vet){
+    
+}
+
+function relat3(vet){
+    
+}
+
+function relat4(vet){
+    
+}

3b2098c227179cebadc95642ef805baa3a3e6316
João Costa <josercosta0223@gmail.com>
2020-08-26 22:03:59 -0300

    Exercicio 9 - Finalizado

--- Lista1-Exe9.js
+++ Lista1-Exe9.js
@@ -1,3 +1,23 @@
-chefe()[
-    
-]
+chefe()
+    function chefe(){
+        var numero = Number(prompt(`informe um número`))
+        exe9(numero)
+    }
+
+    function exe9(num){
+        var soma = 0
+        for (var divisor = num; dividor >= 1; divisor--){
+            if(num % divisor == 0){
+                soma = soma + divisor
+            }
+        }
+
+    //  for (var divisor = 1; dividor <= num; divisor++){
+    //      if(num % divisor == 0){
+    //        soma = soma + divisor
+    //      }
+    //  }
+
+        console.log(`A soma dos dividores de ${num} é ${soma}`)
+
+    }

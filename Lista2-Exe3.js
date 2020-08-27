3e705794954a45c607e6d109292134a802c76ca6
João Costa <josercosta0223@gmail.com>
2020-08-26 22:31:48 -0300

    Exercicio 3 - Criando

--- Lista2-Exe3.js
+++ Lista2-Exe3.js
@@ -2,11 +2,17 @@ chefe()
 
 function chefe(){
     var vetor = []
-    entrada(vetor)
-    relat1(vetor)
-    relat2(vetor)
-    relat3(vetor)
-    relat4(vetor)
+    do{
+        var opcao = Number(prompt(`Digite [1].Entrada | [2].Relat1 | [3].Relat2 | [4].Relat3 | [5].Relat4 | [6].Sair`))
+    switch(opcao){
+        case 1: entrada(vetor)
+        case 2: relat1(vetor)
+        case 3: relat2(vetor)
+        case 4: relat3(vetor)
+        case 5: relat4(vetor)
+        }
+    }
+    while (opcao != 6)
 }
 
 function relat1(vet){

f73060fb9cfa1cd05d93fedc97e9ca81d10033c6
João Costa <josercosta0223@gmail.com>
2020-08-26 21:49:55 -0300

    Exercicio 7 - Finalizado

--- Lista1-Exe7.js
+++ Lista1-Exe7.js
@@ -3,23 +3,27 @@ chefe()
 function chefe(){
     var vetor = [] // Declaro o Vetor
     exe7(vetor) // Estamos passando o vetor como parametro - Escopo Global
+    console.log(vetor)
+    calculaMedia(vetor)
 }
 
+//entrada de dados
 function exe7(vet){ // Vet representa vetor -> O que fizermos com o vet, será em vetor
 
-    var numero = prompt(`Informe um número`)
+    var numero = Number(prompt(`Informe um número`))
     do {
-        // Colo o numero em Vet
+        // Coloca o numero em Vet
         vet.push(numero)
-        numero = prompt(`Informe outro número | Número negativo para encerrar.`)
+        numero = Number(prompt(`Informe outro número | Número negativo para encerrar.`))
     }
     while (numero >= 0)
 }
 
+//calcula e mostra a media
 function calculaMedia(vet){
     var soma = 0
-    for (var i=0; i < vet.legnth; i++){
+    for (var i=0; i < vet.length; i++){
         soma = soma + vet[i]
     }
-    console.log(`A Média é: ${soma/vet.le}`)
+    console.log(`A Média é: ${soma/vet.length}`)
 }
